3bf3766054a517d4c7898cf5cae43696ca7af937
DESKTOP-2COU9M7\asus <klaimoatez@gmail.com>
2018-04-22 13:00:38 +0200

    formulaire example 1

--- appAngular5/src/app/app.module.ts
+++ appAngular5/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { Example1Component } from './formulaire/example1/example1.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Example1Component
   ],
   imports: [
     BrowserModule
